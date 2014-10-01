a148db8c80f3b130bc15d021abe6cf4596a883f3
estelle <estelle@estellemac>
2014-10-01 15:10:22 -0400

    Open editor for all property
    Also cleanup error handling and remove fake marker created to open
    editor

--- org.eclipse.cbmc.ui/src/org/eclipse/internal/cbmc/view/OpenEditorAction.java
+++ org.eclipse.cbmc.ui/src/org/eclipse/internal/cbmc/view/OpenEditorAction.java
@@ -2,10 +2,9 @@ package org.eclipse.internal.cbmc.view;
 
 import java.util.HashMap;
 import org.eclipse.cbmc.Property;
-import org.eclipse.cbmc.PropertyStatus;
 import org.eclipse.core.resources.*;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.*;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.viewers.*;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
@@ -17,21 +16,19 @@ public class OpenEditorAction implements IDoubleClickListener {
 		final IStructuredSelection selection = (IStructuredSelection) event.getSelection();
 		if (selection != null && !selection.isEmpty() && selection.getFirstElement() instanceof Property) {
 			Property selectedProperty = (Property) selection.getFirstElement();
-			if (selectedProperty.getStatus() == PropertyStatus.FAILED) {
-				Path path = new Path(selectedProperty.getFile().getName());
-				IFile file = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(path);
-				HashMap<String, Object> map = new HashMap<String, Object>();
-				map.put(IMarker.LINE_NUMBER, selectedProperty.getLine());
-				IMarker marker;
-				try {
-					marker = file.createMarker(IMarker.TEXT);
-					marker.setAttributes(map);
-					IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), marker);
-				} catch (CoreException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
+			Path path = new Path(selectedProperty.getFile().getName());
+			IFile file = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(path);
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put(IMarker.LINE_NUMBER, selectedProperty.getLine());
+			IMarker marker;
+			try {
+				marker = file.createMarker(IMarker.TEXT);
+				marker.setAttributes(map);
+				IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(), marker);
+				marker.delete();
+			} catch (CoreException e) {
+				ErrorDialog.openError(null, "Open editor failed", "A problem occurred opening the editor associated with this property.", new Status(IStatus.ERROR, org.eclipse.internal.cbmc.Activator.PLUGIN_ID, "Problem opening editor", e)); //$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$
+				return;
 			}
 		}
 	}
