3bd71d578075aa8f2cd7b94b17be546692e3bb14
Pascal Rapicault <pascal@rapicorp.com>
2014-10-08 21:28:48 -0400

    Add logging and enable it

--- org.eclipse.cbmc.ui/src/org/eclipse/internal/cbmc/view/CheckPropertyJob.java
+++ org.eclipse.cbmc.ui/src/org/eclipse/internal/cbmc/view/CheckPropertyJob.java
@@ -2,7 +2,10 @@ package org.eclipse.internal.cbmc.view;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.eclipse.cbmc.*;
 import org.eclipse.cbmc.util.CBMCCliHelper;
 import org.eclipse.core.runtime.*;
@@ -13,6 +16,7 @@ public class CheckPropertyJob extends Job {
 
 	private static final String COUTEREXAMPLE_EXT = ".xml"; //$NON-NLS-1$
 	private static final String FILE_COUNTEREXAMPLE_PREFIX = "counterexample."; //$NON-NLS-1$
+	private static final Logger logger = Logger.getLogger(CheckPropertyJob.class.getName());
 
 	private Property property;
 	private File counterExampleFile;
@@ -32,7 +36,9 @@ public class CheckPropertyJob extends Job {
 			property.setStatus(PropertyStatus.RUNNING);
 			CBMCCliHelper cliHelper = ((Results) property.eContainer()).getCBMCHelper();
 			counterExampleFile = new File(cliHelper.getWorkingDirectory(), FILE_COUNTEREXAMPLE_PREFIX + property.getNumber() + COUTEREXAMPLE_EXT);
-			ProcessBuilder pb = new ProcessBuilder(cliHelper.getCommandLineForPropertyCheck(property));
+			ArrayList<String> cli = cliHelper.getCommandLineForPropertyCheck(property);
+			logger.log(Level.INFO, "Checking property " + cli);
+			ProcessBuilder pb = new ProcessBuilder(cli);
 			pb.redirectErrorStream(true);
 			pb.redirectOutput(counterExampleFile);
 			Process process = pb.start();
--- org.eclipse.cbmc.ui/src/org/eclipse/internal/cbmc/view/GeneratePropertiesJob.java
+++ org.eclipse.cbmc.ui/src/org/eclipse/internal/cbmc/view/GeneratePropertiesJob.java
@@ -2,6 +2,9 @@ package org.eclipse.internal.cbmc.view;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
@@ -19,6 +22,7 @@ public class GeneratePropertiesJob extends Job {
 	private static final String CLAIMS_OUTPUT_CBMC = "claims.output.cbmc"; //$NON-NLS-1$
 	private static final String CLAIMS_INPUT_XML = "claims.input.xml"; //$NON-NLS-1$
 	private static final String TRANSFORM_XSL = "cbmcTransform.xsl"; //$NON-NLS-1$
+	private static final Logger logger = Logger.getLogger(GeneratePropertiesJob.class.getName());
 
 	private CBMCCliHelper cliHelper;
 
@@ -37,12 +41,15 @@ public class GeneratePropertiesJob extends Job {
 
 			//Execute cbmc to generate all properties
 			//Here we are not trying to cancel because the generation of the property is fast
-			ProcessBuilder pb = new ProcessBuilder(cliHelper.getCommandLineForAllProperties());
+			ArrayList<String> cli = cliHelper.getCommandLineForAllProperties();
+			logger.log(Level.INFO, "Executing " + cli);
+			ProcessBuilder pb = new ProcessBuilder(cli);
 			pb.redirectErrorStream(true);
 			pb.redirectOutput(inputfile);
 			Process process = pb.start();
 			process.waitFor();
 
+			logger.log(Level.INFO, "Transforming CBMC output: " + inputfile.getAbsolutePath() + " into " + outputfile.getAbsolutePath());
 			//Transform CBMC XML output into something EMF can read
 			Source xmlInput = new StreamSource(inputfile);
 			Source xsl = new StreamSource(FileLocator.openStream(Platform.getBundle(Activator.PLUGIN_ID), new Path(TRANSFORM_XSL), false));
@@ -50,12 +57,14 @@ public class GeneratePropertiesJob extends Job {
 			Transformer transformer = TransformerFactory.newInstance().newTransformer(xsl);
 			transformer.transform(xmlInput, xmlOutput);
 
+			logger.log(Level.INFO, "Loading CBMC results as a model");
 			//Load newly created file
 			URI uri = URI.createFileURI(outputfile.getAbsolutePath());
 			ResourceSet resSet = new ResourceSetImpl();
 			Resource resource = resSet.getResource(uri, true);
 			results = (Results) resource.getContents().get(0);
 			results.setCBMCHelper(cliHelper);
+			logger.log(Level.INFO, "Loading CBMC results as a model succeeded");
 		} catch (IOException | TransformerException | InterruptedException e) {
 			return new Status(IStatus.ERROR, Activator.PLUGIN_ID, Messages.CBMC_execution_error, e);
 		}
