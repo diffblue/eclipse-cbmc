f8748f6b7912c0020186892e129aba507d20e40f
Pascal Rapicault <pascal@rapicorp.com>
2014-11-20 11:37:26 -0500

    Missed file

--- org.eclipse.cbmc.tracedebugger/src-main/results/data/Vars.java
+++ org.eclipse.cbmc.tracedebugger/src-main/results/data/Vars.java
@@ -2,6 +2,7 @@ package results.data;
 
 import java.util.List;
 
+import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
 
 
@@ -18,5 +19,19 @@ public class Vars {
 	public String has_more;
 	public String format;
 	public String exp;
-	public List<Child> children;
+	
+	//This method exists because the serialization format of the children is not standard 
+	//[child={name="var2.x",exp="x",numchild="0",type="int",thread-id="1"},child={name="var2.y",exp="y",numchild="0",type="int",thread-id="1"},child={name="var2.z",exp=\"z",numchild="0",type="int",thread-id="1"}],has_more="0"
+	public static String serializeAsChildrenList(List<Vars> children) {
+		StringBuilder result = new StringBuilder(200);
+		result.append('[');
+		for (Vars child : children) {
+			result.append("child=");
+			result.append(new Gson().toJson(child));
+			result.append(',');
+		}
+		result.deleteCharAt(result.length() - 1);
+		result.append(']');
+		return result.toString();
+	}
 }
