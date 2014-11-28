b65db628124bf395bf8342bc5df61312ffca60b0
Pascal Rapicault <pascal@rapicorp.com>
2014-11-27 21:35:18 -0500

    Cleanup

--- org.eclipse.cbmc.tracedebugger.test/src/org/eclipse/cbmc/tracedebugger/test/Chatter.java
+++ org.eclipse.cbmc.tracedebugger.test/src/org/eclipse/cbmc/tracedebugger/test/Chatter.java
@@ -17,18 +17,13 @@ import java.util.regex.Pattern;
 public class Chatter {
 	private static final String INPUT_FILE_MARKER = "<<inputFile>>";
 
-	enum LineKind {
-		command, response;
-	}
-	
 	File input;
 	FileWriter outputStream;
 	Map<String, List<String>> responses = new HashMap<>();
 
-	LineKind lastLineKind = LineKind.command;
 	String commandId = "";
 	String counterExamplePath;
-	
+
 	public Chatter(File filename, String counterExample) throws IOException {
 		counterExamplePath = counterExample;
 		input = File.createTempFile("cbmcTest", "txt");
@@ -39,11 +34,11 @@ public class Chatter {
 		scanner.close();
 		outputStream.close();
 	}
-	
+
 	private void readData(Scanner scan) throws IOException {
 		Pattern pattern = Pattern.compile("([0-9]+)-(\\S+)(.*)");
-		
-		while(scan.hasNextLine()) {
+
+		while (scan.hasNextLine()) {
 			String line = scan.nextLine();
 			if (line.trim().length() == 0)
 				continue;
@@ -53,13 +48,12 @@ public class Chatter {
 			} else {
 				addResponse(line);
 			}
-			
+
 		}
 		scan.close();
 	}
-	
+
 	private void addCommand(String line) throws IOException {
-		lastLineKind = LineKind.command;
 		commandId = "";
 		Pattern pattern = Pattern.compile("([0-9]+)(.*)");
 		Matcher matches = pattern.matcher(line);
@@ -70,30 +64,20 @@ public class Chatter {
 	}
 
 	private void addResponse(String line) {
-		try {
-			if ("(gdb)".equals(line))
-				return;
-//			Pattern pattern = Pattern.compile("([0-9]+)\\^(\\S+)");
-//			Matcher matches = pattern.matcher(line);
-			String resultToInsert = null;
-//			if (matches.find()) {
-//				resultToInsert  = matches
-//			} else {
-				resultToInsert = line;
-//			}
-			resultToInsert = resultToInsert.replaceAll(INPUT_FILE_MARKER, counterExamplePath);
-			List<String> value = responses.getOrDefault(commandId, new ArrayList<String>());
-			value.add(resultToInsert);
-			responses.put(commandId, value);
-		} finally {
-			lastLineKind = LineKind.response;
-		}
+		if ("(gdb)".equals(line))
+			return;
+		String resultToInsert = null;
+		resultToInsert = line;
+		resultToInsert = resultToInsert.replaceAll(INPUT_FILE_MARKER, counterExamplePath);
+		List<String> value = responses.getOrDefault(commandId, new ArrayList<String>());
+		value.add(resultToInsert);
+		responses.put(commandId, value);
 	}
 
 	public File getInput() throws IOException {
 		return input;
 	}
-	
+
 	public Map<String, List<String>> getResponses() {
 		return responses;
 	}
