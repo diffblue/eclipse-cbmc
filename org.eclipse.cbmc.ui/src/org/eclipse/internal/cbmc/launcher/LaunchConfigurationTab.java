a8dcafa4ec6a0ba87ce58b61eb1a3ea0337a32df
Pascal Rapicault <pascal@rapicorp.com>
2014-12-06 23:03:07 -0500

    Remove unnecessary default constructor

--- org.eclipse.cbmc.ui/src/org/eclipse/internal/cbmc/launcher/LaunchConfigurationTab.java
+++ org.eclipse.cbmc.ui/src/org/eclipse/internal/cbmc/launcher/LaunchConfigurationTab.java
@@ -4,10 +4,6 @@ import org.eclipse.debug.ui.*;
 
 public class LaunchConfigurationTab extends AbstractLaunchConfigurationTabGroup {
 
-	public LaunchConfigurationTab() {
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
 		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {new MainLaunchingTab()};
