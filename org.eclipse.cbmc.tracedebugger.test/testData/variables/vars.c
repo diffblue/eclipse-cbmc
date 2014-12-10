683a63b7560eb895b470cde866fe7b159d44b6bf
Pascal Rapicault <pascal@rapicorp.com>
2014-12-10 12:22:50 -0500

    Fix up variable test

--- org.eclipse.cbmc.tracedebugger.test/testData/variables/vars.c
+++ org.eclipse.cbmc.tracedebugger.test/testData/variables/vars.c
@@ -4,7 +4,7 @@ struct sss {
 	int i;
 	struct ssss {
 		int g;
-		int innerArray[10];
+		int innerArray[2];
 	} b;
 };
 
@@ -12,15 +12,11 @@ int main() {
 	int i = 1;
 	i++;
 
-	int arri[2];
-	arri[0] = 1;
-	arri[1] = 2;
-
 	struct sss myStruct;
 	myStruct.i = 2;
 	myStruct.b.g = 3;
 	myStruct.b.innerArray[0]=7;
 	myStruct.b.innerArray[1]=7;
 
-	assert(arri[1] == 1);
+	assert(0);
 }
